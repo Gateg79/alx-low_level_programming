065e0f708f5da81beee21b8a9a433099c815d178
Gateg79 <edikan.fred@gmail.com>
2022-04-10 20:10:18 -0700

    Size declaration in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
+/**
+ * main - sizes or data types are summarized
+ * Return: 0
+ */
 int main(void)
 {
 
 	char c;
-	int i
+	int i;
 	long int a;
 	long long int v;
 	float f;
 
-	printf("Size of a char: %lu.\n" (unsigned long)sizeof(c));
-	printf("Size of an int: %lu.\n" (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu.\n" (unsigned long)sizeof(a));
-	printf("Size of a long long int: %lu.\n" (unsigned long)sizeof(v));
-	printf("Size of a float: %lu.\n" (unsigned long)sizeof(f));
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu.\n", (unsigned long) sizeof(a));
+	printf("Size of a long long int: %lu.\n", (unsigned long) sizeof(v));
+	printf("Size of a float: %lu.\n", (unsigned long) sizeof(f));
 
 	return(0);
 }

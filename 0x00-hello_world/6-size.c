07d7e926cd08ea724760de35adb50490b660da7d
Gateg79 <edikan.fred@gmail.com>
2022-04-10 19:10:46 -0700

    Size command in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+	char c;
+	int i
+	long int a;
+	long long int v;
+	float f;
+
+	printf("Size of a char: %lu.\n" (unsigned long)sizeof(c));
+	printf("Size of an int: %lu.\n" (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu.\n" (unsigned long)sizeof(a));
+	printf("Size of a long long int: %lu.\n" (unsigned long)sizeof(v));
+	printf("Size of a float: %lu.\n" (unsigned long)sizeof(f));
+
+	return(0);
+}
+

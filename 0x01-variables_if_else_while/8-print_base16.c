0122eba74f3b83f71a5cae3c157d4c672a8cb715
Gateg79 <edikan.fred@gmail.com>
2022-04-11 21:15:52 -0700

    New C statements

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: base 16 in lowercase print statement
+ * Return: 0
+ */
+int main(void)
+{
+	int i, y;
+	for (i = 0; i < 10; i++)
+	{
+		putchar (i + '0');
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar (y);
+	}
+
+	putchar('\n');
+	return (0);
+}

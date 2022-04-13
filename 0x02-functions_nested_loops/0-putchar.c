2b99307713b2206fdc9001daa58b482d489cab33
Gateg79 <edikan.fred@gmail.com>
2022-04-13 04:24:02 -0700

    New

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - main bloch=k
+ * Return:0
+ */
+int main(void)
+{
+char c[10] = "Main";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return(0);
+}

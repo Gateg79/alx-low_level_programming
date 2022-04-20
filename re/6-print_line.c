fe810df4cdf3aecfd6468e9a6483a6e6ae4625b3
Gateg79 <edikan.fred@gmail.com>
2022-04-19 19:31:43 -0700

    New

---
+++ re/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - print line n times
+ *
+ * Return:void
+ */
+
+void print_line(int n)
+{
+	
+	int i = 0;
+
+	while (i < n && n > 0)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+
+}

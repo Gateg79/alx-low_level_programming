625f1711634a19f44999c0d68dd98b63b5b52218
Gateg79 <edikan.fred@gmail.com>
2022-04-21 23:11:49 -0700

    New

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int c = 0;
 	int sum = 0;
+	c++;
 
 	while (c < 1024)
 	{
 		if (c % 3 == 0 || c % 5 == 0)
 		{
-			sum += c; c++;
+			sum += c;
+		
 		}
 	}
 	printf("%i/n", sum);

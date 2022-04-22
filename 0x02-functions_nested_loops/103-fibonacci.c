a91b663eaedbfb15da087d25540dcd78ab514cb4
Gateg79 <edikan.fred@gmail.com>
2022-04-21 23:23:31 -0700

    New

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * main - main void
- * Description: computes and prints even number < 4,000,000
- * 5 below 1024 (exclude)
- * Return; 0
+ * main - fibonacci numbers
+ * Description: computes and prints even number
+ * below 1024 (exclude)
+ * Return: 0
  */
 int main(void)
 {
 	int a = 0, b = 1, next = 0;
 	int sum = 0;
-
 	while (next < 4000000)
 	{
 		next = a + b;

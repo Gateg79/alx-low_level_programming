3bb1fd3cfd43bac4d715a80d4b2dbcaac0f0804e
Gateg79 <edikan.fred@gmail.com>
2022-04-24 12:42:47 -0700

    New

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strncat - appends src to the dest string
+ * *_strncat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
  * @n: largest bytes to append

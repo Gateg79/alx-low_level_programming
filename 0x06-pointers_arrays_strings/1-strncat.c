46c7b9708d418df0f55c9637fce4ec0b36d6e445
Gateg79 <edikan.fred@gmail.com>
2022-04-24 12:40:42 -0700

    New

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+int i = 0, j = 0;
 
-	while (*dest + i)
-		i++;
-	while (j < n && *(src + j))
-	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
-	}
-	if (j < n)
-		*(dest + i) = *(src + j);
-	return (dest);
+while (*dest + i)
+	i++;
+while (j < n && *(src + j))
+{
+	*(dest + i) = *(src + j);
+	i++;
+	j++;
+}
+if (j < n)
+	*(dest + i) = *(src + j);
+return (dest);
 }

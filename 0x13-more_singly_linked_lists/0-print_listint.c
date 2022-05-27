f773bf430f62d6e25f5d73f21197d337c4c0f69e
Gateg79 <edikan.fred@gmail.com>
2022-05-26 20:54:06 -0700

    Singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t s = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		s++;
+		count++;
 	}
-	return (s);
+	return (count);
 }

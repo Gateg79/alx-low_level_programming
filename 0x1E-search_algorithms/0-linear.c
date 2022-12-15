7a4e4d45be5ca72c5cf496766214be0d394dbe49
Gateg79 <edikan.fred@gmail.c>
2022-12-15 15:57:41 -0800

    New

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,16 +14,16 @@
 int linear_search(int *array, size_t size, int value)
 {
 	int b;
-
+	
 	if (array == NULL)
 		return (-1);
-
+	
 	for (b = 0; b < (int)size; b++)
 	{
 		printf("Value checked array[%u] = [%d]\n", b, array[b]);
 		if (value == array[b])
 			return (b);
 	}
-
+	
 	return (-1);
 }
